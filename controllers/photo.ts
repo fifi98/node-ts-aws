e85e58e289437beb38d2f47d20f350e3190f87a6
fifi98 <filip.bel@hotmail.com>
2021-05-13 14:43:42 +0200

    Implement uploading photo to S3 bucket

--- controllers/photo.ts
+++ controllers/photo.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import { UploadedFile } from "express-fileupload";
+import { nanoid } from "nanoid";
 
-export const upload = (req: Request, res: Response) => {
-  res.status(200).json({ success: true });
+import s3 from "../config/s3";
+
+export const upload = async (req: Request, res: Response) => {
+  try {
+    if (!req.files) return res.status(400).json({ success: false, error: "Missing photo" });
+
+    const photo = req.files.photo as UploadedFile;
+    const { name, author } = req.body;
+
+    const filename = nanoid();
+
+    // Upload photo
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME as string,
+      Body: photo.data,
+      Key: `photos/${filename}`,
+      ACL: "public-read",
+      ContentType: "image/jpeg",
+    };
+
+    await s3.upload(params).promise();
+
+    // Store to database
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
 };
